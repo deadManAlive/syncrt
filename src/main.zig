c61a769443d49033c16bc2885f20fda7ec55cd73
deadManAlive <alfarrakhan@gmail.com>
2023-04-07 22:50:06 +0700

    can read file

--- src/main.zig
+++ src/main.zig
@@ -1,12 +1,17 @@
 const std = @import("std");
 const process = std.process;
+const fs = std.fs;
+const io = std.io;
 
 pub fn main() !void {
+    // ?
     var arena = std.heap.ArenaAllocator.init(std.heap.page_allocator);
     defer arena.deinit();
 
+    // ??
     const alloc = arena.allocator();
 
+    // ???
     const args = try process.argsAlloc(alloc);
     defer process.argsFree(alloc, args);
 
@@ -15,5 +20,18 @@ pub fn main() !void {
         return error.InvalidArgs;
     }
 
-    std.debug.print("filename: {s}\n", .{args[1]});
+    const file_name = args[1];
+
+    std.debug.print("filename: {s}\n", .{file_name});
+
+    var file_handle = try fs.cwd().openFile(file_name, .{});
+    defer file_handle.close();
+
+    var buf_reader = io.bufferedReader(file_handle.reader());
+    var in_stream = buf_reader.reader();
+
+    var buffer: [1024]u8 = undefined;
+    while (try in_stream.readUntilDelimiterOrEof(&buffer, '\n')) |line| {
+        std.debug.print("{s}\n", .{line});
+    }
 }
